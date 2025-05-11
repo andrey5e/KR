0b0a064aeb385287a0595b0df5d2d0f597b23cda
andrey5e <161711012+andrey5e@users.noreply.github.com>
2025-05-11 15:11:55 +0300

    start player.h

--- player.h
+++ player.h
@@ -1,4 +1,21 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include "board.h"
+#include "Defines.h"
+#include "shotsstrategy.h"
+
+class Player {
+public:
+    Player();
+    virtual ~Player();
+    virtual Ship* createShip(int w) = 0;
+    void createFlot();
+    Board *getBoard();
+    QPoint performShot(QPoint point = QPoint(-1, -1));
+protected:
+    Board* board;
+    ShotsStrategy* shotS;
+};
+
 #endif // PLAYER_H
