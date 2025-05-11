106669f4a26f815963a85f6cd26a0beefe3926e2
andrey5e <161711012+andrey5e@users.noreply.github.com>
2025-05-11 15:18:38 +0300

    start mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,22 +2,21 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "controller.h"
+#include <QMouseEvent>
+#include <QPixMap>
+#include <QPainter>
+#include <QImage>
+#include <QVector>
+#include <QKeyEvent>
+#include <QApplication>
 
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
-QT_END_NAMESPACE
-
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
-
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    QPoint getCoordsPlayer(int x, int y);
+    QPoint getCoords(int x, int y, int fieldX, int fieldY);
 
-private:
-    Ui::MainWindow *ui;
-};
 #endif // MAINWINDOW_H
