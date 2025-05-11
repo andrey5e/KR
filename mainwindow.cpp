eb9f478476453ff78f652985baa8f9e3c8fb4c66
andrey5e <161711012+andrey5e@users.noreply.github.com>
2025-05-11 15:23:59 +0300

    start mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,23 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-}
+    : QMainWindow(parent) {
+    setFixedSize(555, 309);
+    gameController = new GameController();
+        
+    // Отладка
+    qDebug() << "Главное окно создано, контроллер инициализирован";
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+    // Создание фона
+    QPalette pal;
+    pal.setBrush(QPalette::Active, QPalette::Window, QBrush(QPixmap(":/images/background.png")));
+    this->setPalette(pal);
+
+    // Интеграция кнопки
+    gameController->infoLabel = new QLabel(this);
+    gameController->infoLabel->move(200, 260);
+    gameController->infoLabel->setText("Расставьте корабли!");
+    gameController->infoLabel->setFixedSize(170, 40);
+    gameController->infoLabel->setStyleSheet("font-weight: bold; border-style: outset; border-width: 2px; border-radius: 10px;");
+    gameController->infoLabel->setAlignment(Qt::AlignCenter);
 }
