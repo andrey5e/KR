ad54e1d47ec3727c434616d8c189e9add8761f1a
andrey5e <161711012+andrey5e@users.noreply.github.com>
2025-05-11 15:05:16 +0300

    start shot.h

--- shot.h
+++ shot.h
@@ -1,4 +1,14 @@
 #ifndef SHOT_H
 #define SHOT_H
 
+#include <QPoint>
+#include <QDebug>
+
+class ShotsStrategy
+{
+public:
+    // функция будет просто возвращать QPoint, а в gameController уже будет все обрабатываться
+    virtual QPoint shot(QPoint point = QPoint(0, 0)) = 0;
+};
+
 #endif // SHOT_H
