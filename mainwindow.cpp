c67b40401aaf303dbd8705ef64cfffe52cd1c5db
andrey5e <161711012+andrey5e@users.noreply.github.com>
2025-05-11 15:27:08 +0300

    mousePressEvent

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -37,3 +37,87 @@ QPoint MainWindow::getCoords(int x, int y, int fieldX, int fieldY) {
     res.setY(1.0*(y-fieldY)/cfy);
     return res;
 }
+
+void MainWindow::mousePressEvent(QMouseEvent *event) {
+    
+    // Обработка нажатия правой кнопкой мыши
+    if (event->button() == Qt::RightButton) {
+        QPoint pos = event->pos();
+        qDebug() << "Mouse click at (" << pos.x() << "," << pos.y() << ")";
+        qDebug() << "\nПоле бота:";
+        gameController->printBotAllCellStates();
+    }
+        
+    // Обработка нажатия левой кнопкой мыши
+    else if (event->button() == Qt::LeftButton) {
+        
+        // Состояние = расстановка кораблей
+        if (gameController->getGameState() == GameState::SHIPS_PLACING) {
+            QPointF pos = event->position();
+            if (pos.x() >= MYFIELD_X && pos.x() <= FIELD_WIDTH + MYFIELD_X
+                && pos.y() >= MYFIELD_Y && pos.y() <= FIELD_HEIGHT + MYFIELD_Y) {
+                QPoint qp = getCoords(pos.x(), pos.y(), MYFIELD_X, MYFIELD_Y);
+                if (qp.x() == 10)
+                    qp.setX(9);
+                if (qp.y() == 10)
+                    qp.setY(9);
+                if (gameController->isPlayerEmptyCell(qp)) {
+                    if (gameController->getPlayerShipCellsCount() < 20) {
+                        gameController->setPlayerCellState(qp, 2);
+                        if (gameController->checkPlayerShipPlacement()) {
+                            gameController->infoLabel->setText("Начать - ПРОБЕЛ");
+                        }
+                    }
+                } else {
+                    gameController->setPlayerCellState(qp, 0);
+                    gameController->infoLabel->setText("Расставьте корабли!");
+                }
+                update();
+            }
+        } else if (gameController->getGameState() == GameState::PLAYER_TURN) {
+            QPointF pos = event->position();
+            if (pos.x() >= ENEMYFIELD_X && pos.x() <= FIELD_WIDTH + ENEMYFIELD_X
+                && pos.y() >= ENEMYFIELD_Y && pos.y() <= FIELD_HEIGHT + ENEMYFIELD_Y) {
+                QPoint qp = getCoords(pos.x(), pos.y(), ENEMYFIELD_X, ENEMYFIELD_Y);
+                if (qp.x() == 10)
+                    qp.setX(9);
+                if (qp.y() == 10)
+                    qp.setY(9);
+                gameController->playerShot(qp);
+                repaint();
+                QApplication::processEvents();
+
+                // Проверка: завершение игры
+                int gO_status = gameController->checkForGameOver();
+                if (gameController->checkForGameOver() != 0) {
+                    if (gO_status == 2) {
+                        gameController->setGameState(GameState::GAMEOVER);
+                        gameController->infoLabel->setText("Вы Победили!");
+                    } else {
+                        gameController->setGameState(GameState::GAMEOVER);
+                        gameController->infoLabel->setText("Вы Проиграли!");
+                    }
+                }
+
+                while (gameController->getGameState() == GameState::ENEMY_TURN) {
+                    // ход бота
+                    gameController->botShot();
+                    repaint();
+                    QApplication::processEvents();
+
+                    // проверка на завершение игры
+                    int gO_status = gameController->checkForGameOver();
+                    if (gameController->checkForGameOver() != 0) {
+                        if (gO_status == 2) {
+                            gameController->setGameState(GameState::GAMEOVER);
+                            gameController->infoLabel->setText("Вы Победили!");
+                        } else {
+                            gameController->setGameState(GameState::GAMEOVER);
+                            gameController->infoLabel->setText("Вы Проиграли!");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
