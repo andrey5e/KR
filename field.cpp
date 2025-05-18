a1d08e2961f6d416061e8cf94d1b84e232679e4a
andrey5e <161711012+andrey5e@users.noreply.github.com>
2025-05-18 14:23:20 +0300

    start field.cpp

--- field.cpp
+++ field.cpp
@@ -1,3 +1,10 @@
 #include "field.h"
 
-field::field() {}
+Board::Board() {
+    cells.fill(EMPTY, 100); // Инициализация игрового поля 10x10, заполняя все ячейки состоянием EMPTY
+}
+
+// Добавление корабля в флот
+void Board::addShip(Ship *ship) {
+    flot.push_back(ship);
+}
