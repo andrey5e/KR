c557d0a43f0cd255dc0ff538491099bece46ff5b
andrey5e <161711012+andrey5e@users.noreply.github.com>
2025-05-18 13:59:15 +0300

    adding comments

--- controller.h
+++ controller.h
@@ -1,6 +1,10 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
+#include "player.h"
+#include <unistd.h>
+#include <QLabel>
+
 enum GameState {
     SHIPS_PLACING,
     PLAYER_TURN,
@@ -12,32 +16,32 @@ class GameController {
 public:
     GameController();
     ~GameController();
-    GameState getGameState();
-    void createFlots();
-    int getPlayerShipCellsCount();
-    int getBotShipCellsCount();
-    bool isPlayerEmptyCell(QPoint point);
-    bool isBotEmptyCell(QPoint point);
-    void setPlayerCellState(QPoint point, int stateNum);
-    void setBotCellState(QPoint point, int stateNum);
-    Cell getBotCellState(QPoint point);
-    Cell getPlayerCellState(QPoint point);
-    void printPlayerAllCellStates();
-    void printBotAllCellStates();
-    QVector<Cell> getPlayerAllCells();
-    QVector<Cell> getBotAllCells();
-    bool checkPlayerShipPlacement();
-    bool checkBotShipPlacement();
-    void syncPlayerShipsCells();
-    void syncBotShipsCells();
-    void setGameState(GameState newState);
-    void botRandomShipsPlacing();
-    void playerShot(QPoint point);
-    void botShot();
+    GameState getGameState(); // Получение текущего состояния игры
+    void createFlots(); // Создание флотов для игрока и бота
+    int getPlayerShipCellsCount(); // Получение количества ячеек с кораблями у игрока
+    int getBotShipCellsCount(); // Получение количества ячеек с кораблями у бота
+    bool isPlayerEmptyCell(QPoint point); // Проверка, является ли ячейка пустой у игрока
+    bool isBotEmptyCell(QPoint point); // Проверка, является ли ячейка пустой у бота
+    void setPlayerCellState(QPoint point, int stateNum); // Установка состояния ячейки на доске у игрока
+    void setBotCellState(QPoint point, int stateNum); // Установка состояния ячейки на доске у бота
+    Cell getPlayerCellState(QPoint point); // Получение состояния ячейки на доске у игрока
+      Cell getBotCellState(QPoint point); // Получение состояния ячейки на доске у бота
+    void printPlayerAllCellStates(); // Печать всех состояний ячеек на доске у игрока
+    void printBotAllCellStates(); // Печать всех состояний ячеек на доске у бота
+    QVector<Cell> getPlayerAllCells(); // Получение всех ячеек на доске у игрока
+    QVector<Cell> getBotAllCells(); // Получение всех ячеек на доске у бота
+    bool checkPlayerShipPlacement(); // Проверка размещения кораблей у игрока
+    bool checkBotShipPlacement(); // Проверка размещения кораблей у бота
+    void syncPlayerShipsCells(); // Синхронизация ячеек кораблей у игрока
+    void syncBotShipsCells(); // Синхронизация ячеек кораблей у бота
+    void setGameState(GameState newState); // Установка нового состояния игры
+    void botRandomShipsPlacing(); // Размещение кораблей бота случайным образом
+    void playerShot(QPoint point); // Обработка выстрела игрока
+    void botShot(); // Обработка выстрела бота
     QLabel *infoLabel;
-    void takeShot(Player* whoShots, Player* whoseField, QPoint point);
-    void swapGameState();
-    int checkForGameOver();
+    void takeShot(Player* whoShots, Player* whoseField, QPoint point); // Обработка выстрела
+    void swapGameState(); // Переключение состояния игры между ходами игрока и бота
+    int checkForGameOver(); // Проверка на окончание игры
 private:
     GameState gameState;
     Player* player;
