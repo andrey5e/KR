e6203240976fb985cb17ee58d129ab7db805eba7
andrey5e <161711012+andrey5e@users.noreply.github.com>
2025-05-18 15:01:12 +0300

    adding comments

--- shot.cpp
+++ shot.cpp
@@ -5,6 +5,7 @@ QPoint ManualShotStrategy::shot(QPoint point) {
     return point;
 }
 
+// Случайный выстрел
 QPoint FullRandomShotStrategy::shot(QPoint point) {
     srand(time(NULL));
     return QPoint(rand() % 10, rand() % 10);
