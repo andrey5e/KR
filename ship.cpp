679a3bf851d9651795812e007fa9091d4b943c92
andrey5e <161711012+andrey5e@users.noreply.github.com>
2025-05-18 14:54:41 +0300

    adding comments

--- ship.cpp
+++ ship.cpp
@@ -1,21 +1,26 @@
 #include "ship.h"
 
+// Получение веса корабля
 int Ship::getWeight() {
     return weight;
 }
 
+// Установка координат корабля
 void Ship::setCoords(QPoint point) {
     coords = point;
 }
 
+// Получение текущего здоровья корабля
 int Ship::getHealth() {
     return shipHealth;
 }
 
+// Получение координат корабля
 QPoint Ship::getCoords() {
     return coords;
 }
 
+// Нанесение урона кораблю
 void Ship::shipDamage() {
     shipHealth--;
 }
