0e5e6461905b3305419c7c660721308e058642de
andrey5e <161711012+andrey5e@users.noreply.github.com>
2025-05-18 14:24:02 +0300

    basic methods

--- field.cpp
+++ field.cpp
@@ -8,3 +8,29 @@ Board::Board() {
 void Board::addShip(Ship *ship) {
     flot.push_back(ship);
 }
+
+// Установка состояния ячейки на игровом поле
+void Board::setCellState(QPoint point, Cell state) {
+    if (point.x() < 10 && point.x() >= 0 && point.y() < 10 && point.y() >= 0)
+        cells[point.x() + 10 * point.y()] = state;
+}
+
+// Получение состояния ячейки на игровом поле
+Cell Board::getCellState(QPoint point) {
+    if (point.x() > 9 || point.y() > 9 || point.x() < 0 || point.y() < 0) {
+        return Cell::EMPTY;
+    }
+    return cells[point.x() + 10 * point.y()];
+}
+
+// Вывод состояния игрового поля в виде матрицы
+void Board::printBoardStates() {
+    for (int i = 0; i < 10; i++) {
+        QString str = "";
+        for (int j {0}; j < 10; j++) {
+            str += QString::number(cells[i * 10 + j]) + " ";
+        }
+        qDebug() << str;
+    }
+    qDebug() << "\n";
+}
