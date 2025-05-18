affa002879be42ffe5dfea6ee31ff581e5781ddf
andrey5e <161711012+andrey5e@users.noreply.github.com>
2025-05-18 14:52:58 +0300

    adding comments  and refactoring

--- ship.h
+++ ship.h
@@ -6,48 +6,51 @@
 
 class Ship {
 protected:
-    int weight;
-    QPoint coords;
-    int shipHealth;
-    bool orientation;
+    int weight; // Вес (размер) корабля
+    QPoint coords; // Координаты расположения корабля на игровом поле
+    int shipHealth; // Здоровье корабля (количество оставшихся палуб)
+    bool orientation; // Ориентация корабля (горизонтальная или вертикальная)
 public:
-    Ship() {};
-    int getWeight();
-    void setCoords(QPoint point);
-    int getHealth();
-    QPoint getCoords();
-    void shipDamage();
+    int getWeight(); // Получение веса корабля
+    void setCoords(QPoint point); // Установка координат корабля
+    int getHealth(); // Получение текущего здоровья корабля
+    QPoint getCoords(); // Получение координат корабля
+    void shipDamage(); // Нанесения урона кораблю
 };
 
+// Однопалубный корабль
 class OnePartShip : public Ship {
 public:
     OnePartShip() {
-        weight = 1;
-        shipHealth = weight;
+        weight = 1; // Вес корабля
+        shipHealth = weight; // Здоровье = вес
     }
 };
 
+// Двухпалубный корабль
 class TwoPartShip : public Ship {
 public:
     TwoPartShip() {
-        weight = 2;
-        shipHealth = weight;
+        weight = 2; // Вес корабля
+        shipHealth = weight; // Здоровье = вес
     }
 };
 
+// Трёхпалубный корабль
 class ThreePartShip : public Ship {
 public:
     ThreePartShip() {
-        weight = 3;
-        shipHealth = weight;
+        weight = 3; // Вес корабля
+        shipHealth = weight; // Здоровье = вес
     }
 };
 
+// Четырёхпалубный корабль
 class FourPartShip : public Ship {
 public:
     FourPartShip() {
-        weight = 4;
-        shipHealth = weight;
+        weight = 4; // Вес корабля
+        shipHealth = weight; // Здоровье = вес
     }
 };
 
