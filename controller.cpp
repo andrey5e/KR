2e90ea9f5bcbc7c9ae8653b15a11ebe9972794ae
andrey5e <161711012+andrey5e@users.noreply.github.com>
2025-05-18 14:09:52 +0300

    start controller.cpp

--- controller.cpp
+++ controller.cpp
@@ -1,3 +1,13 @@
 #include "controller.h"
 
-controller::controller() {}
+GameController::GameController() {
+    gameState = GameState::SHIPS_PLACING;
+    player = new HumanPlayer(); // Создание игрока
+    bot = new AIPlayer(); // Создание бота
+    createFlots(); // Создание флотов для игрока и бота
+}
+
+GameController::~GameController() {
+    delete player; // Удаление игрока
+    delete bot; // Удаление бота
+}
